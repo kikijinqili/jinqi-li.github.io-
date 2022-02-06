6d2761035611423f682cc9c39de47e72fc964e7f
kikijinqili <jinqi.li.310@gmail.com>
2022-02-06 15:27:33 -0800

    title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: Jinqi Li
+ * @Date: 2020-09-13 20:53:51
+ * @LastEditors: Jinqi Li
+ * @LastEditTime: 2022-02-06 15:20:41
+ * @FilePath: /gatsby-portfolio/src/pages/index.js
+ */
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import App from '../components/App';
@@ -12,9 +19,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Jinqi Li'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Jinqi Li'} />
       </Helmet>
       <App />
     </>
